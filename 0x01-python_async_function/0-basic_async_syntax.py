24fc4b7a58a142208cee1ca20136b2e4c02c4c6e
solaure2tt <solaure2t@yahoo.fr>
2023-10-11 16:17:49 +0000

    update documentation task 0 and 3

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,6 +10,13 @@ from typing import Generator
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """ asynchronous coroutine that waits for a random delay
+        and eventually returns it
+        args:
+          max_delay (int): max delay
+        return:
+            the delay
+    """
     result = random.uniform(0, max_delay)
     await asyncio.sleep(result)
     return result
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,11 +5,11 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task :
-    """ asyncio.Task
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ creat an asyncio.Task from wait_random
         args:
-           max_delay (int): delays to create a task
+          max_delay (int): delays to create a task
         return:
-             asyncio.Task
+          asyncio.Task
     """
     return asyncio.create_task(wait_random(max_delay))
